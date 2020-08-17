16715516d82a4bcc5c751ca101963b15378b40ea
schimpfen <nathanschimpf@gmail.com>
2020-08-17 09:43:51 -0400

    Read and Plot Trackpoints from flight-csv's

--- read_IFF_Data.py
+++ read_IFF_Data.py
@@ -1,3 +1,95 @@
-import numpy as np
+import os
 
-iff = np.genfromtxt("Data/IFF_USA_20200601_050022_86377.csv",delimiter=",",skip_header=1000,max_rows=1000)
+os.environ['PROJ_LIB'] = 'C:\\Users\\natha\\anaconda3\\envs\\WeatherPreProcessing\\Library\\share'
+"""
+Read Flight Track-Point Files and Plot in Basemap
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+from mpl_toolkits.basemap import Basemap, addcyclic
+from scipy.ndimage.filters import minimum_filter, maximum_filter
+from matplotlib import dates, cm
+from netCDF4 import Dataset, num2date
+import EchoTop_Data_Tools as et
+
+
+
+def extrema(mat, mode='wrap', window=10):
+    """find the indices of local extrema (min and max)
+    in the input array."""
+    mn = minimum_filter(mat, size=window, mode=mode)
+    mx = maximum_filter(mat, size=window, mode=mode)
+    # (mat == mx) true if pixel is equal to the local max
+    # (mat == mn) true if pixel is equal to the local in
+    # Return the indices of the maxima, minima
+    return np.nonzero(mat == mn), np.nonzero(mat == mx)
+
+
+
+# create Basemap instance.
+m = Basemap(projection='merc', llcrnrlat=24., urcrnrlat=50., \
+            llcrnrlon=-123., urcrnrlon=-67., resolution='c', \
+            rsphere=et.R_EARTH, lat_0=40., lon_0=-98., lat_ts=20.)
+
+# draw boundary, fill continents, draw costlines, draw parrallels, draw meridians
+m.drawmapboundary()
+m.drawcoastlines(linewidth=1.25)
+m.drawparallels(np.arange(10, 60, 10), labels=[1, 0, 0, 0])
+m.drawmeridians(np.arange(-160, -50, 10), labels=[0, 0, 0, 1])
+
+# plot 00 UTC today.
+# date = datetime.now().strftime('%Y%m%d')+'00'
+
+# Open, plot, and downsample each flight-track CSV
+os.chdir('data/IFF_Track_Points')
+for file in os.listdir('data/IFF_Track_Points'):
+    data = np.loadtxt(file, delimiter=',', usecols=(1, 2, 3, 4))
+    print(data[0:10])
+
+    # read lats,lons,alts.
+    lats = data[:, 1]
+    lons = data[:, 2]
+    alts = data[:, 3]
+    times = data[:, 0]
+    print('Latitude:', lats[0:10])
+    print('Longitude:', lons[0:10])
+    print('Altitude:', alts[0:10])
+    print('Times:', times[0:10])
+
+    # the window parameter controls the number of highs and lows detected.
+    # (higher value, fewer highs and lows)
+    #local_min, local_max = extrema(alts, mode='wrap', window=50)
+
+    # adjust time
+    # time units: seconds since 1970-01-01T00:00:00Z
+    # time calendar: gregorian
+    unit = "seconds since 1970-01-01T00:00:00"
+    calendar1 = "gregorian"
+    timestamps = num2date(times, units="seconds since 1970-01-01T00:00:00", calendar="gregorian")
+    print('Timestamps:', timestamps[0])
+
+    # generate meshgrid to plot contour-map
+    lonsm, latsm = np.meshgrid(lons, lats)
+    altsm = np.zeros(np.shape(latsm))
+
+    for i in range(0,len(lats)):
+        altsm[i][i] = alts[i]
+
+    m.contour(lonsm, latsm, altsm, latlon=True, cmap=cm.coolwarm)
+
+
+# Return to Project Directory
+os.chdir('../../')
+
+#TODO: Pull in flightplan data and parse lat/lon with openNav
+
+# plot show
+plt.title('JFK-LAX Flights, Mercator Projection')
+m.colorbar(mappable=None, location='right', size='5%', pad='1%')
+plt.show(block=False)
+plt.savefig("Output/IFF_Flight_Track.png", format='png')
+plt.close()
+
--- read_NC_Data.py
+++ read_NC_Data.py
@@ -36,69 +36,52 @@ for v in [x0, y0, z0, echotop, echotop_f, time]:
 dates = num2date(time[:], units=time.units, calendar=time.calendar)
 
 
-# check for poor echotop readings based on status flags
-# NO LONGER USED
-arr_echotop_f = echotop_f[::]
-#arr_temp = arr_echotop_f.flat
-#for temp in arr_temp:
-#    if (temp == 1 or temp == 2):
-#        print("FLAG: ",temp)
+# Plot EchoTop Readings, Raw
 
-
-arr_echotop = echotop[::]
-#for temp in arr_echotop.flat:
-#    if (temp != -1000.):
-#        print("ALT: ",temp)
-
-# Plot EchoTop Readings
-#TODO: Map Coordinates matching
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-xm,ym = np.meshgrid(x0,y0)
-surf = ax.plot_surface(xm,ym,echotop[0][0],cmap=cm.coolwarm)
+xm,ym = np.meshgrid(x0, y0)
+surf = ax.plot_surface(xm, ym, echotop[0][0], cmap=cm.coolwarm)
 plt.title("EchoTops for " + time.string)
 ax.view_init(elev=90,azim=-90)
 fig.colorbar(surf, shrink=0.5, aspect=5)
 plt.show(block=False)
-plt.savefig("Output/EchoTop_Raw.png",format='png')
+plt.savefig("Output/EchoTop_Raw.png", format='png')
 plt.close()
 
+
 # Create Basemap, plot on Latitude/Longitude scale
-m = Basemap(width=12000000,height=9000000,rsphere=et.R_EARTH, \
-            resolution='l',area_thresh=1000.,projection='lcc',\
-            lat_0=et.LAT_ORIGIN,lon_0=et.LONG_ORIGIN)
-#m.drawmapboundary(fill_color='aqua')
-#m.fillcontinents(color='coral',lake_color='aqua')
+m = Basemap(width=12000000, height=9000000, rsphere=et.R_EARTH, \
+            resolution='l', area_thresh=1000., projection='lcc',\
+            lat_0=et.LAT_ORIGIN, lon_0=et.LONG_ORIGIN)
 m.drawcoastlines()
 
 
 # Draw Meridians and Parallels
 Parallels = np.arange(0.,80.,10.)
 Meridians = np.arange(10.,351.,20.)
-#Labels = [left,right,top,bottom]
+
+# Labels = [left,right,top,bottom]
 m.drawparallels(Parallels,labels=[False,True,True,False])
 m.drawmeridians(Meridians,labels=[True,False,False,True])
 fig2 = plt.gca()
 
-# Map to Lambert Conformal Projection
+
+# Map EchoTop x,y to Lambert Conformal Projection
 # x0,y0: meters from lat:38 long:-90
 # xlat,ylong: equivalent lat/long values
-# x1,y1: mapped lat/long values to basemap figure
-#TODO: more efficient method, search docs
-ylat, xlong = et.relToLatLong(x0[:],y0[:],et.LAT_ORIGIN,et.LONG_ORIGIN,et.R_EARTH)
-xlongm,ylatm = np.meshgrid(xlong,ylat)
-#x1,y1 = m(xlongm,ylatm)
 
-# define filled contour levels
+#TODO: try using LatLon objects
+ylat, xlong = et.relToLatLong(x0[:], y0[:], et.LAT_ORIGIN, et.LONG_ORIGIN, et.R_EARTH)
+xlongm, ylatm = np.meshgrid(xlong, ylat)
+
+
+# Define filled contour levels and plot
 clevs = np.arange(-1e3,10e3,1e3)
-#xcontour = np.reshape(x1,-1)
-#ycontour = np.reshape(y1,-1)
-#et_contour = np.reshape(echotop[0][0],-1)
-ET_Lambert_Contour = m.contourf(xlongm,ylatm,echotop[0][0],clevs,latlon=True,cmap=cm.coolwarm)
-m.colorbar(ET_Lambert_Contour,location='right',pad='5%')
+ET_Lambert_Contour = m.contourf(xlongm, ylatm, echotop[0][0], clevs, latlon=True, cmap=cm.coolwarm)
+m.colorbar(ET_Lambert_Contour, location='right', pad='5%')
 plt.show(block=False)
 plt.savefig("Output/EchoTop_Projected.png",format='png')
 plt.close()
+
 rootgrp.close()
-
-
