c340ea2f6e2763f381a74a6652f586f00745e20c
Nathan Schimpf <ntschi01@louisville.edu>
2021-05-07 10:38:10 -0400

    Weather_Cubes.py via UTM Projections

--- Weather_Cubes.py
+++ Weather_Cubes.py
@@ -4,11 +4,13 @@ import math, os, datetime, logging
 #from mpl_toolkits.basemap import Basemap
 #from matplotlib import pyplot as plt
 from netCDF4 import Dataset, num2date, date2num
+import utm
 from dateutil import parser as dparser
 from concurrent.futures import ProcessPoolExecutor
 from functools import partial
 from numba import jit
-import numba
+#from Global_Tools import profile
+
 '''
 EXCLUDE PROFILER
 from Global_Tools import profile
@@ -60,7 +62,7 @@ def get_relevant_timestamps(flt_startdate, flt_enddate, flt_time, file, var, PAT
     return cur_timestamps, fore_timestamps, idx_fore_day_split, idx_cur_day_split, PATH_DATA_CUR_DATE, PATH_DATA_FORE_DATE
 
 #@jit()
-def get_axes(lats, lons, flt_lat, flt_lon, heading):
+def get_axes(lats, lons, flt_lat, flt_lon, heading, dist_m):
     heading_ortho = (heading + 90) % 360
     theta = math.radians(heading - 90)
     theta_ortho = math.radians(heading_ortho - 90)
@@ -84,34 +86,81 @@ def get_axes(lats, lons, flt_lat, flt_lon, heading):
 
     diff_lon, diff_lat = np.array(np.abs(lons - flt_lon)), np.array(np.abs(lats - flt_lat))
     #data_delta_lon, data_delta_lat = diff_lon[np.nonzero(diff_lon)].min(), diff_lat[np.nonzero(diff_lat)].min()
-    data_delta_lat, data_delta_lon = gb.latlon_unitsteps(flt_lat,flt_lon,heading,dist_m=1500)
+    data_delta_lat, data_delta_lon = gb.latlon_unitsteps(flt_lat,flt_lon,heading,dist_m=dist_m)
 
-    unitstep_x = data_delta_lon / np.cos(theta)
-    unitstep_y = data_delta_lat / np.sin(theta)
-    unitstep_ortho_x = data_delta_lon / np.cos(theta_ortho)
-    unitstep_ortho_y = data_delta_lat / np.sin(theta_ortho)
+    unitstep_x = data_delta_lon * np.cos(theta)
+    unitstep_y = data_delta_lat * np.sin(theta)
+    unitstep_ortho_x = data_delta_lon * np.cos(theta_ortho)
+    unitstep_ortho_y = data_delta_lat * np.sin(theta_ortho)
     return unitstep_x, unitstep_y, unitstep_ortho_x, unitstep_ortho_y
 
 @jit(nopython=True)
+def gridded_crop(lats, lons, relevant_data, lat_center, lon_center, data_dims, min_degree = 2.):
+    lats, lons, relevant_data = lats.flatten(), lons.flatten(), relevant_data.reshape(data_dims[0], data_dims[1],data_dims[2],-1)
+
+    idx_min_lats = np.where(np.abs(lats - lat_center) < min_degree)[0]
+    idx_min_lons = np.where(np.abs(lons - lon_center) < min_degree)[0]
+
+    idx_common_idxs = np.array([x for x in idx_min_lats if x in idx_min_lons])
+
+    lats_cropped = lats[idx_common_idxs]
+    lons_cropped = lons[idx_common_idxs]
+    data_cropped = relevant_data[:,:,:,idx_common_idxs]
+    #lats_cropped = np.pa
+
+    return lats_cropped, lons_cropped, data_cropped
+
+
+@jit(nopython=True, nogil=True)
 def fill_cube(weather_cube_proj: np.array, relevant_data: np.array, lats: np.array, lons: np.array,
-              len_lookahead: numba.int32, len_prds: numba.int32, len_alts: numba.int32):
-    weather_cube_actual = np.zeros((int(2), gb.CUBE_SIZE, gb.CUBE_SIZE), dtype=numba.float64)
+              len_lookahead: np.int32, len_prds: np.int32, len_alts: np.int32):
+    weather_cube_actual = np.zeros((int(2), gb.CUBE_SIZE, gb.CUBE_SIZE), dtype=np.float64)
     #Cube Dims (lookahead x products x height x lat x lon) (t,v,z,lat,lon)
-    weather_cube_data = np.zeros((len_lookahead,len_prds,len_alts, gb.CUBE_SIZE, gb.CUBE_SIZE), dtype=numba.float64)
+    weather_cube_data = np.zeros((len_lookahead,len_prds,len_alts, gb.CUBE_SIZE, gb.CUBE_SIZE), dtype=np.float64)
 
     for idx_ in range(0, gb.CUBE_SIZE):
         for idx_ortho in range(0, gb.CUBE_SIZE):
             haversine_matrix = gb.haversine(weather_cube_proj[1][idx_][idx_ortho], lats, lons - weather_cube_proj[0][idx_][idx_ortho])
             idx_point = np.where(np.abs(haversine_matrix).min() == np.abs(haversine_matrix))
             idx_point = (idx_point[0][0],idx_point[1][0])
+            #idx_point = idx_point[0][0]
             weather_cube_actual[0][idx_][idx_ortho] = lons[idx_point]
             weather_cube_actual[1][idx_][idx_ortho] = lats[idx_point]
 
             #collect all t, v, alt for the lat/lon point
             weather_cube_data[:,:,:,idx_,idx_ortho] = relevant_data[:,:,:,idx_point[0],idx_point[1]]
+            #weather_cube_data[:,:,:,idx_,idx_ortho] = relevant_data[:,:,:,idx_point]
             '''for t in range(0, len(gb.LOOKAHEAD_SECONDS)):
                 for v in range(0,len(prd['products'])):
                     weather_cube_data[t,v,:,idx_,idx_ortho] = relevant_data[t,v,:,idx_point[0],idx_point[1]]'''
+    return weather_cube_actual, weather_cube_data
+
+def fill_cube_utm(weather_cube_proj: np.array, relevant_data: np.array, UTM_dict: dict, UTM_traceback_dict: dict,
+                  lats: np.array, lons: np.array, len_lookahead: np.int32, len_prds: np.int32, len_alts: np.int32):
+    weather_cube_actual = np.zeros((int(2), gb.CUBE_SIZE, gb.CUBE_SIZE), dtype=np.float64)
+    #Cube Dims (lookahead x products x height x lat x lon) (t,v,z,lat,lon)
+    weather_cube_data = np.zeros((len_lookahead,len_prds,len_alts, gb.CUBE_SIZE, gb.CUBE_SIZE), dtype=np.float64)
+    for idx_ in range(0, gb.CUBE_SIZE):
+        for idx_ortho in range(0, gb.CUBE_SIZE):
+            proj_lon, proj_lat = weather_cube_proj[0][idx_][idx_ortho], weather_cube_proj[1][idx_][idx_ortho]
+            wc_proj_northing, wc_proj_easting, wc_proj_zonenum, wcproj_zonechar = \
+                utm.from_latlon(proj_lat,proj_lon)
+
+            # Find coordinates for relevant UTM region
+            grpkey = '{}-{}'.format(wcproj_zonechar, wc_proj_zonenum)
+            assert grpkey in UTM_dict.keys(), 'Flight UTM Grid {} Does Not Exist'.format(grpkey)
+            dist_matrix = gb.utm_dist(wc_proj_northing, wc_proj_easting, UTM_dict[grpkey][0],UTM_dict[grpkey][1])
+            idx_point = np.where(np.abs(dist_matrix).min() == np.abs(dist_matrix))
+            #idx_point = (idx_point[0][0],idx_point[1][0])
+            idx_point = idx_point[0][0]
+            x,y = UTM_traceback_dict[grpkey][0,idx_point],UTM_traceback_dict[grpkey][1,idx_point]
+            act_lat, act_lon = lats[x,y], lons[x,y]
+            '''act_lat, act_lon = utm.to_latlon(UTM_dict[grpkey][0][idx_point], UTM_dict[grpkey][1][idx_],
+                                             int(grpkey.split('-')[1]), grpkey.split('-')[0])'''
+            weather_cube_actual[1][idx_][idx_ortho] = act_lat
+            weather_cube_actual[0][idx_][idx_ortho] = act_lon
+            #collect all t, v, alt for the lat/lon point
+            weather_cube_data[:,:,:,idx_,idx_ortho] = relevant_data[:,:,:,x,y]
     return weather_cube_actual, weather_cube_data
 
 def process_flight_plan(prd, USES_CUR, USES_FORE, fore_start, file):
@@ -208,7 +257,8 @@ def process_flight_plan(prd, USES_CUR, USES_FORE, fore_start, file):
             heading = gb.heading_a_to_b(flt_lon[i-1], flt_lat[i-1], flt_lat[i], flt_lon[i])
         else:
             heading = gb.heading_a_to_b(flt_lon[i], flt_lat[i], flt_lat[i + 1], flt_lon[i + 1])
-        unitstep_x, unitstep_y, unitstep_ortho_x, unitstep_ortho_y = get_axes(prd['lats'], prd['lons'], flt_lat[i], flt_lon[i], heading)
+        unitstep_x, unitstep_y, unitstep_ortho_x, unitstep_ortho_y = get_axes(prd['lats'], prd['lons'],
+                                                              flt_lat[i], flt_lon[i], heading, prd['spatial res'])
 
         # Generate 20-point axis orthogonal to heading
         centerline_ortho_x, actual_ortho_delta_x = np.linspace(- (gb.CUBE_SIZE / 2) * unitstep_ortho_x,
@@ -245,9 +295,25 @@ def process_flight_plan(prd, USES_CUR, USES_FORE, fore_start, file):
 
         weather_cube_alt = prd['alts'][idx_alt-1:idx_alt+2]
 
-        weather_cube_actual, weather_cube_data = fill_cube(weather_cube_proj, relevant_data, prd['lats'], prd['lons'],
-                                                           len(gb.LOOKAHEAD_SECONDS), len(prd['products']),prd['cube height'])
+        '''
+        lats_cropped, lons_cropped, data_cropped = gridded_crop(prd['lats'], prd['lons'], relevant_data,
+                weather_cube_proj[1][int(gb.CUBE_SIZE/2)][int(gb.CUBE_SIZE/2)], weather_cube_proj[0][int(gb.CUBE_SIZE/2)][int(gb.CUBE_SIZE/2)],
+                np.array([len(gb.LOOKAHEAD_SECONDS), len(prd['products']), prd['cube height']]))
 
+        squaredim = int(np.ceil(np.sqrt(len(lats_cropped))))
+        lats_cropped = np.resize(lats_cropped, (squaredim,squaredim))
+        lons_cropped = np.resize(lons_cropped, (squaredim,squaredim))
+        data_cropped = np.resize(data_cropped, (data_cropped.shape[0], data_cropped.shape[1], data_cropped.shape[2], squaredim,squaredim))
+        '''
+
+        weather_cube_actual, weather_cube_data = fill_cube_utm(weather_cube_proj, relevant_data, prd['UTM'], prd['UTM-latlon idxs'],
+                           prd['lats'],prd['lons'], len(gb.LOOKAHEAD_SECONDS), len(prd['products']),prd['cube height'])
+
+        #weather_cube_actual, weather_cube_data = fill_cube(weather_cube_proj, relevant_data, prd['lats'], prd['lons'],
+        #                                                   len(gb.LOOKAHEAD_SECONDS), len(prd['products']),prd['cube height'])
+
+        #weather_cube_actual, weather_cube_data = fill_cube(weather_cube_proj, data_cropped, lats_cropped, lons_cropped,
+        #                                                   len(gb.LOOKAHEAD_SECONDS), len(prd['products']),prd['cube height'])
 
         # Print the max Error between cube points
         #if i % 100 == 0:
@@ -257,8 +323,10 @@ def process_flight_plan(prd, USES_CUR, USES_FORE, fore_start, file):
         print("{}\tMax Distance Err:\t".format(datetime.datetime.now()), "{:10.4f}\t".format(maxerr), "\t", str(i + 1),
               ' / ', len(flight_tr[:, 1] - 1), '\t', file.split('/')[-1])
 
+        if i == len(flt_time):
+            exit(13)
+
         # Append current cube to list of data
-        #TODO: Optimize, stack?
         weather_cubes_lat[i] = weather_cube_actual[1]
         weather_cubes_lon[i] = weather_cube_actual[0]
         weather_cubes_alt[i] = weather_cube_alt
@@ -286,6 +354,7 @@ def process_flight_plan(prd, USES_CUR, USES_FORE, fore_start, file):
 
     # Add Dimensions
     cubes_rootgrp.createDimension('time', size=None)
+    cubes_rootgrp.createDimension('lookahead',size=len(gb.LOOKAHEAD_SECONDS))
     cubes_rootgrp.createDimension('XPoints', size=gb.CUBE_SIZE)
     cubes_rootgrp.createDimension('YPoints', size=gb.CUBE_SIZE)
     cubes_rootgrp.createDimension('ZPoints',size=prd['cube height'])
@@ -294,27 +363,31 @@ def process_flight_plan(prd, USES_CUR, USES_FORE, fore_start, file):
     cubes_rootgrp.createVariable('time', datatype=float, dimensions=('time'))
     cubes_rootgrp.variables['time'].units = 'Seconds since 1970-01-01T00:00:00'
     cubes_rootgrp.variables['time'].calendar = 'gregorian'
+    cubes_rootgrp.createVariable('lookahead',datatype=float,dimensions=('lookahead'))
+    cubes_rootgrp.variables['lookahead'].units = 'Seconds ahead of current time'
     cubes_rootgrp.createVariable('XPoints', datatype=float, dimensions=('XPoints'))
     cubes_rootgrp.variables['XPoints'].units = 'indexing for each weather cube'
     cubes_rootgrp.createVariable('YPoints', datatype=float, dimensions=('YPoints'))
     cubes_rootgrp.variables['YPoints'].units = 'indexing for each weather cube'
-    cubes_rootgrp.createVariable('Latitude', datatype=float, dimensions=('time', 'XPoints', 'YPoints'))
-    cubes_rootgrp.createVariable('Longitude', datatype=float, dimensions=('time', 'XPoints', 'YPoints'))
-    cubes_rootgrp.createDimension('altitudes', datatype=float, dimensions=('time','ZPoints'))
-    cubes_rootgrp.createVariable('data', datatype=float, dimensions=('time', 'ZPoints', 'XPoints', 'YPoints'))
-    cubes_rootgrp.variables['data'].products = prd['products']
+    cubes_rootgrp.createVariable('latitude', datatype=float, dimensions=('time', 'XPoints', 'YPoints'))
+    cubes_rootgrp.createVariable('longitude', datatype=float, dimensions=('time', 'XPoints', 'YPoints'))
+    cubes_rootgrp.createVariable('altitudes', datatype=float, dimensions=('time','ZPoints'))
+    for prod in prd['products']:
+        cubes_rootgrp.createVariable(prod, datatype=float, dimensions=('time', 'lookahead', 'ZPoints', 'XPoints', 'YPoints'))
+
 
     # Add Metadata: Flight Callsign, Earth-radius,
-    cubes_rootgrp.Callsign = file.split('_')[3]
+    cubes_rootgrp.Callsign = file.split('_')[-1].split('.')[0]
     cubes_rootgrp.rEarth = gb.R_EARTH
 
     # Assign Weather Cube Data to netCDF Variables
     cubes_rootgrp.variables['XPoints'][:] = np.arange(0, gb.CUBE_SIZE, 1)
     cubes_rootgrp.variables['YPoints'][:] = np.arange(0, gb.CUBE_SIZE, 1)
     cubes_rootgrp.variables['time'][:] = weather_cubes_time
-    cubes_rootgrp.variables['Latitude'][:] = weather_cubes_lat
-    cubes_rootgrp.variables['Longitude'][:] = weather_cubes_lon
-    cubes_rootgrp.variables[prd['products']][:] = weather_cubes_data
+    cubes_rootgrp.variables['latitude'][:] = weather_cubes_lat
+    cubes_rootgrp.variables['longitude'][:] = weather_cubes_lon
+    for p in range(len(prd['products'])):
+        cubes_rootgrp.variables[prd['products'][p]][:] = weather_cubes_data[:,:,p,:,:,:]
 
     cubes_rootgrp.close()
     return 0
@@ -322,37 +395,48 @@ def process_flight_plan(prd, USES_CUR, USES_FORE, fore_start, file):
 
 def main():
     # open sample Trajectory and Echotop data
-    PATH_COORDS = gb.PATH_PROJECT + '/Data/IFF_Flight_Plans/Sorted/'
+    PATH_COORDS = gb.PATH_PROJECT + '/Data/IFF_Flight_Plans/Interpolated/'
 
     et_sample = Dataset('Data/EchoTop/Sorted/2019-01-10/Current/EchoTop.20190110T000000Z.nc','r', format='NETCDF4')
     vil_sample = Dataset('Data/VIL/Sorted/2019-01-10/Current/VIL.2019-01-10T000000Z.nc','r',format='NETCDF4')
     hrrr_sample = Dataset('Data/HRRR/Sorted/2019-01-10/Current/hrrr.2019-01-10T000000Z.wrfprsf00.nc')
+    ciws_lats, ciws_lons = np.array(et_sample['lats'][:]), np.array(et_sample['lons'][:])
+    ciws_utmdict, ciws_traceback, df_ciws_verif = gb.longrange_latlon_to_utm(ciws_lats, ciws_lons)
+    df_ciws_verif.to_csv(gb.PATH_PROJECT + '/Output/Weather Cubes/CIWS_UTM.csv')
+    hrrr_lats, hrrr_lons = np.array(hrrr_sample['lats'][:]), np.array(hrrr_sample['lons'][:])
+    hrrr_utmdict, hrrr_traceback, df_hrrr_verif = gb.longrange_latlon_to_utm(hrrr_lats, hrrr_lons)
+    df_hrrr_verif.to_csv(gb.PATH_PROJECT + '/Output/Weather Cubes/HRRR_UTM.csv')
 
-    prd_et = {'products': ['ECHO_TOP'], 'cube height': 1, 'lats': et_sample['lats'][:], 'lons': et_sample['lons'][:],
-              'alts': et_sample['alt'][:], 'sorted path': gb.PATH_PROJECT + '/Data/EchoTop/Sorted/',
-              'output path': gb.PATH_PROJECT + '/Output/Weather Cubes/ECHO_TOP/', 'refresh rate': 150,
+    prd_et = {'products': ['ECHO_TOP'], 'cube height': 1, 'lats': ciws_lats, 'lons': ciws_lons,
+              'UTM': ciws_utmdict, 'UTM-latlon idxs': ciws_traceback,
+              'alts': np.array(et_sample['alt'][:]), 'sorted path': gb.PATH_PROJECT + '/Data/EchoTop/Sorted/',
+              'output path': gb.PATH_PROJECT + '/Output/Weather Cubes/ECHO_TOP/', 'refresh rate': 150,'spatial res': 1850,
               'log path': gb.PATH_PROJECT + '/Output/Weather Cubes/ECHO_TOP_Cube_Gen.log'}
-    prd_vil = {'products': ['VIL'], 'cube height': 1, 'lats': vil_sample['lats'][:], 'lons': vil_sample['lons'][:],
-               'alts': vil_sample['alt'][:], 'sorted path': gb.PATH_PROJECT + '/Data/VIL/Sorted/',
-               'output path': gb.PATH_PROJECT + '/Output/Weather Cubes/VIL/', 'refresh rate': 150,
+
+    prd_vil = {'products': ['VIL'], 'cube height': 1, 'lats': ciws_lats, 'lons': ciws_lons,
+               'UTM': ciws_utmdict, 'UTM-latlon idxs': ciws_traceback,
+               'alts': np.array(vil_sample['alt'][:]), 'sorted path': gb.PATH_PROJECT + '/Data/VIL/Sorted/',
+               'output path': gb.PATH_PROJECT + '/Output/Weather Cubes/VIL/', 'refresh rate': 150, 'spatial res': 1850,
                'log path': gb.PATH_PROJECT + '/Output/Weather Cubes/VIL_Cube_Gen.log'}
-    prd_hrrr = {'products': ['uwind','vwind','tmp'], 'cube height': 3, 'lats': hrrr_sample['lats'][:], 'lons': hrrr_sample['lons'][:],
-                'alts': hrrr_sample['alt'][:], 'sorted path': gb.PATH_PROJECT + '/Data/HRRR/Sorted/',
-                'output path': gb.PATH_PROJECT + '/Output/Weather Cubes/HRRR/', 'refresh rate': 3600,
+
+    prd_hrrr = {'products': ['uwind','vwind','tmp'], 'cube height': 3, 'lats': hrrr_lats, 'lons': hrrr_lons,
+                'UTM': hrrr_utmdict, 'UTM-latlon idxs': hrrr_traceback,
+                'alts': np.array(hrrr_sample['alt'][:]), 'sorted path': gb.PATH_PROJECT + '/Data/HRRR/Sorted/',
+                'output path': gb.PATH_PROJECT + '/Output/Weather Cubes/HRRR/', 'refresh rate': 3600, 'spatial res': 3000,
                 'log path': gb.PATH_PROJECT + '/Output/Weather Cubes/HRRR_Cube_Gen.log'}
 
     et_sample.close(); vil_sample.close(); hrrr_sample.close();
 
-    #for product in [prd_et, prd_vil, prd_hrrr]:
-    for product in [prd_hrrr]:
-        fmode = 'w'
-        if os.path.isfile(product['log path']):
-            overwrite = input("{} exists: overwrite? [y/n]".format(product['log path']))
-            if overwrite.lower() == 'y':
-                fmode = 'w'
-            elif overwrite.lower() == 'n':
-                fmode == 'a'
-        logging.basicConfig(filename=product['log path'], filemode=fmode, level=logging.INFO)
+    fmode = 'w'
+    if os.path.isfile(prd_hrrr['log path']):
+        overwrite = input("{} exists: overwrite? [y/n]".format(prd_hrrr['log path']))
+        if overwrite.lower() == 'y':
+            fmode = 'w'
+        elif overwrite.lower() == 'n':
+            fmode == 'a'
+    logging.basicConfig(filename=prd_hrrr['log path'], filemode=fmode, level=logging.INFO)
+
+    for product in [prd_hrrr, prd_et, prd_vil]:
 
         USES_CURRENT = gb.LOOKAHEAD_SECONDS.__contains__(0.)
         USES_FORECAST = gb.LOOKAHEAD_SECONDS[len(gb.LOOKAHEAD_SECONDS) - 1] > 0.
